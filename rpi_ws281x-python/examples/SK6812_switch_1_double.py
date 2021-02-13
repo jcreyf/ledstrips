fad46078a671eb3edfb2a8dd3bb7f9e380e9cd0e
Jo Creyf <jo.creyf@gmail.com>
2021-02-12 19:41:53 -0800

    Doing some cleanup

--- rpi_ws281x-python/examples/SK6812_switch_1_double.py
+++ rpi_ws281x-python/examples/SK6812_switch_1_double.py
@@ -12,8 +12,9 @@ from RPi import GPIO
 from rpi_ws281x import Color, PixelStrip, ws
 
 # LED strip configuration:
-LED_COUNT = 215       # Number of LED pixels.
-LED_PIN = 18          # GPIO pin connected to the pixels (must support PWM!).
+#   215 leds on a single strip, connected to RPi GPIO 18 (pin 12) 
+LED_COUNT = 215
+LED_PIN = 18
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA = 10          # DMA channel to use for generating signal (try 10)
 LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
@@ -21,7 +22,7 @@ LED_INVERT = False    # True to invert the signal (when using NPN transistor lev
 LED_CHANNEL = 0
 LED_STRIP = ws.SK6812_STRIP_RGBW
 
-# Light on or off:
+# LED strip on or off:
 LIGHT_STATUS = False
 
 # Light switch 1 connected to pin 16 (GPIO 23) and pin 1 (3v3) to give it power through a 12kOhm resistor:
@@ -74,14 +75,14 @@ def toggleLight(strip):
 
 # Main program:
 if __name__ == '__main__':
-  # Create PixelStrip object with appropriate configuration.
+  # Create PixelStrip object with appropriate configuration:
   global strip
   strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP)
-  # Intialize the library (must be called once before other functions).
+  # Intialize the library (must be called once before other functions):
   strip.begin()
 
   GPIO.setmode(GPIO.BCM)
-  # Create button objects for both switches:
+  # Create GPIO button objects for both switches:
   GPIO.setup(SWITCH_1_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
   GPIO.setup(SWITCH_2_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
@@ -92,7 +93,7 @@ if __name__ == '__main__':
 
   try:
     while True:
-      # Infinite loop, checking the button status every so many milliseconds
+      # Infinite loop, checking the button status every so many milliseconds:
       toggleLight(strip)
       sleep(0.5)
 
