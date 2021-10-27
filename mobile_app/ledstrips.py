0cb68f0bf02114fd242d2f8075fdcd054b20538b
Creyf, Jo <jo.creyf@gmail.com>
2021-10-26 21:54:25 -0700

    Removing unused imports

--- mobile_app/ledstrips.py
+++ mobile_app/ledstrips.py
@@ -5,8 +5,6 @@ from kivymd.uix.screen import MDScreen
 from kivymd.app import MDApp
 from kivy.uix.image import Image
 from kivymd.uix.button import MDFillRoundFlatIconButton, MDFillRoundFlatButton
-from kivymd.uix.textfield import MDTextField
-from kivymd.uix.label import MDLabel
 from kivymd.uix.toolbar import MDToolbar
 import urllib.request
 
