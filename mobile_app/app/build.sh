13b57b211c24bb18864cc30956d5c97e8a2a487d
Creyf, Jo <jo.creyf@gmail.com>
2021-10-30 09:57:35 -0700

    Redoing build process

---
+++ mobile_app/app/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Todo: copy the .py file from project folder
+
+# Todo: add a flag to "clean" (delete .buildozer and venv and bin directories before starting
+
+buildozer android debug deploy run
+
