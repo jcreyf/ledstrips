d51aa74bcf061fd64482855dc79482b1bba503f9
Creyf, Jo <jo.creyf@gmail.com>
2021-12-23 19:10:53 -0800

    Making debugging easier

--- ceiling_lights/BehaviorModules.py
+++ ceiling_lights/BehaviorModules.py
@@ -218,15 +218,16 @@ class ChristmassModule(BehaviorModule):
         # Set the LED color buffer value.
         ws.ws2811_led_set(self._channel, i, color)
         # Send the LED color data to the hardware.
-        resp=ws.ws2811_render(self._leds)
+#        resp=ws.ws2811_render(self._leds)
         if resp != ws.WS2811_SUCCESS:
           message=ws.ws2811_get_return_t_str(resp)
           raise RuntimeError(f"ws2811_render failed with code {resp} ({message})")
-        # Optionally slow down the loop:
-        if self._delayMilliseconds > 0:
-          sleep(self._delayMilliseconds / 1000)
+#        # Optionally slow down the loop:
+#        if self._delayMilliseconds > 0:
+#          sleep(self._delayMilliseconds / 1000)
         # Increase offset to animate colors moving.  Will eventually overflow, which is fine.
         offset += 1
+      resp=ws.ws2811_render(self._leds)
     # The loop ended.
     self.log("ending Christmass thread...", debug=True)
     # Turn all the leds off:
